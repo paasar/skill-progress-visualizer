f3f93577546f4d3f41ac4a365de98890ec71ab29
Ari Paasonen <paasar@gmail.com>
2017-05-23 12:27:44 +0300

    Add bars between skills

--- src/skill_progress_visualizer/html.clj
+++ src/skill_progress_visualizer/html.clj
@@ -10,26 +10,33 @@
            :rel "stylesheet"}]
    [:style (slurp "resources/skill-tree-viz.css")]])
 
+(defn- divs-in-between [elem coll]
+  (interpose elem coll))
+
 (defn- create-skill-columns [skill-tree]
   (let [names (map :name skill-tree)
         symbols (map :symbol skill-tree)
         activations (map :activation skill-tree)]
     [:div.skills
      [:div.column.skill-names
-      (map (fn [[name active]]
-             [:div.skill-name {:class (when active "active")} name]) names)]
+      (divs-in-between [:div.empty]
+        (map (fn [[name active]]
+               [:div.skill-name {:class (when active "active")} name]) names))]
      [:div.column.symbols
-      (map (fn [[symbol active]]
-             [:div.symbol {:class (when active "active")} symbol]) symbols)]
+      (divs-in-between [:div.bar-container [:div.bar]]
+        (map (fn [[symbol active]]
+               [:div.symbol {:class (when active "active")} symbol]) symbols))]
      [:div.column.activations
-      (map (fn [activation]
-             [:div.activation
-             (when activation "ACHIEVED")
-             [:div.activation-text activation]]) activations)]]))
+      (divs-in-between [:div.empty]
+        (map (fn [activation]
+               [:div.activation
+                (when activation "ACHIEVED")
+                [:div.activation-text activation]]) activations))]]))
 
 (defn- create-tree [{:keys [name skill-tree]}]
   [:div.tree
-   [:div.category name]
+   [:div.category-container
+    [:div.category name]]
    (create-skill-columns skill-tree)])
 
 (defn- create-trees [trees]
